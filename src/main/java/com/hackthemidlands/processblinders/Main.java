de560a20828631b5238439d3c858f3a67a36e924
MQuay <mquaynor99@outlook.com>
2020-10-22 16:25:33 +0100

    semicolons_smh

--- src/main/java/com/hackthemidlands/processblinders/Main.java
+++ src/main/java/com/hackthemidlands/processblinders/Main.java
@@ -3,6 +3,6 @@ package com.hackthemidlands.processblinders;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Barebones unoffensive project for F*** YEEE")
+        System.out.println("Barebones unoffensive project");
     }
 }
