e8449bac79b1bf037375efd0f63bd6dcecc8ed95
andreeaivancu <73306531+andreeaivancu@users.noreply.github.com>
2020-10-22 18:44:07 +0100

    Update Main.java

--- src/main/java/com/hackthemidlands/processblinders/Main.java
+++ src/main/java/com/hackthemidlands/processblinders/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Barebones unoffensive project");
 	      System.out.println("REUBEN WOZ HERE (again)");
+	System.out.println("HTM{Sm0k3_w33d}");
     }
 }
