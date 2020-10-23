24b298ea3254f3c261fa8e11688c59479da2e92c
rainestormee <ryanarrowsmith01@gmail.com>
2020-10-23 06:27:11 +0100

    countdown

--- src/main/java/com/hackthemidlands/processblinders/TestViewRoute.java
+++ src/main/java/com/hackthemidlands/processblinders/TestViewRoute.java
@@ -1,6 +1,13 @@
 package com.hackthemidlands.processblinders;
 
 import spark.*;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 import java.util.Map;
 import java.util.HashMap;
 
@@ -9,7 +16,9 @@ public class TestViewRoute implements TemplateViewRoute {
     public ModelAndView handle(Request request, Response response) {
         Map<String, Object> model = new HashMap<String, Object>();
         model.put("title", "testing 123");
-        model.put("hotel", "trivago");
+        LocalDateTime dateTime = Instant.ofEpochMilli(System.currentTimeMillis()).atZone(ZoneId.systemDefault()).toLocalDateTime();
+        dateTime = dateTime.plus(1, ChronoUnit.HOURS);
+        model.put("startTime", dateTime.toString());
 
         return new ModelAndView(model, "timer");
     }
