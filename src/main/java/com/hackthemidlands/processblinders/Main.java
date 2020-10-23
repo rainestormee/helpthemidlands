8c5b1087a7be4ffee19b5b5f721e5fe7a1859408
rainestormee <ryanarrowsmith01@gmail.com>
2020-10-23 05:52:01 +0100

    Timer

--- src/main/java/com/hackthemidlands/processblinders/Main.java
+++ src/main/java/com/hackthemidlands/processblinders/Main.java
@@ -2,14 +2,19 @@ package com.hackthemidlands.processblinders;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import spark.Spark;
 
 import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
 
 public class Main {
 
     private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        Spark.exception(Exception.class, (exception, request, response) -> exception.printStackTrace()); // allow spark to internally handle exceptions
+        staticFileLocation("/public");
         get("/hello", (req, res) -> "Hello World");
+        get("/test", (req, res) -> "timer");
     }
 }
