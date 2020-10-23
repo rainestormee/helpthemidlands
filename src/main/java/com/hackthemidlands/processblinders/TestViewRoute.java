e8768736f91cba72933975799a6c37941357c489
rainestormee <ryanarrowsmith01@gmail.com>
2020-10-23 06:44:46 +0100

    maven-compiler-plugin version

--- src/main/java/com/hackthemidlands/processblinders/TestViewRoute.java
+++ src/main/java/com/hackthemidlands/processblinders/TestViewRoute.java
@@ -1,20 +1,21 @@
 package com.hackthemidlands.processblinders;
 
-import spark.*;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.TemplateViewRoute;
 
 import java.time.Instant;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
-import java.util.Calendar;
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class TestViewRoute implements TemplateViewRoute {
 
     public ModelAndView handle(Request request, Response response) {
-        Map<String, Object> model = new HashMap<String, Object>();
+        Map<String, Object> model = new HashMap<>();
         model.put("title", "testing 123");
         LocalDateTime dateTime = Instant.ofEpochMilli(System.currentTimeMillis()).atZone(ZoneId.systemDefault()).toLocalDateTime();
         dateTime = dateTime.plus(1, ChronoUnit.HOURS);
