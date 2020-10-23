05dd5a4c7667c9f1b11b43c7bac982394f880179
rainestormee <ryanarrowsmith01@gmail.com>
2020-10-23 07:04:37 +0100

    Updating timer, padding 0's

--- src/main/java/com/hackthemidlands/processblinders/TestViewRoute.java
+++ src/main/java/com/hackthemidlands/processblinders/TestViewRoute.java
@@ -14,9 +14,12 @@ import java.util.Map;
 
 public class TestViewRoute implements TemplateViewRoute {
 
+    private int drones = 0;
+
     public ModelAndView handle(Request request, Response response) {
         Map<String, Object> model = new HashMap<>();
-        model.put("title", "testing 123");
+        drones++;
+        model.put("drones", drones);
         LocalDateTime dateTime = Instant.ofEpochMilli(System.currentTimeMillis()).atZone(ZoneId.systemDefault()).toLocalDateTime();
         dateTime = dateTime.plus(1, ChronoUnit.HOURS);
         model.put("startTime", dateTime.toString());
