ccbc131607b3606d1120167bece0f9321f60d711
rainestormee <ryanarrowsmith01@gmail.com>
2020-10-23 22:57:50 +0100

    @data

--- src/main/java/com/hackthemidlands/processblinders/api/Order.java
+++ src/main/java/com/hackthemidlands/processblinders/api/Order.java
@@ -1,12 +1,14 @@
 package com.hackthemidlands.processblinders.api;
 
+import lombok.Data;
+
 import java.util.*;
 
+@Data
 public class Order {
     private List <String> shopList = new ArrayList<>();
     private double maxPrice;
     private String location;
     private OrderStatus status;
     //time window?
-
 }
