99d0b0cb3c8c030512e95a95b2793d148655631b
rainestormee <ryanarrowsmith01@gmail.com>
2020-10-23 22:10:15 +0100

    Fixing

--- src/main/java/com/hackthemidlands/processblinders/VolunteerViewRoute.java
+++ src/main/java/com/hackthemidlands/processblinders/VolunteerViewRoute.java
@@ -5,10 +5,12 @@ import spark.Request;
 import spark.Response;
 import spark.TemplateViewRoute;
 
+import java.util.HashMap;
+
 public class VolunteerViewRoute implements TemplateViewRoute {
 
     @Override
     public ModelAndView handle(Request request, Response response) throws Exception {
-        return new ModelAndView(null, "volunteer");
+        return new ModelAndView(new HashMap<>(), "volunteer");
     }
 }
