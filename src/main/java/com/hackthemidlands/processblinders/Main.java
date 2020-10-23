55f684c5a5b3075c7319f29cade07e51602d1040
Reuben Sinclair <reubenjsinclair@gmail.com>
2020-10-23 05:47:57 +0100

    add /goodbye

--- src/main/java/com/hackthemidlands/processblinders/Main.java
+++ src/main/java/com/hackthemidlands/processblinders/Main.java
@@ -5,6 +5,10 @@ import static spark.Spark.*;
 public class Main {
 
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/hello", (req, res) -> "Hello world");
+
+        get("/goodbye/:name", (req,res) -> {
+            return "See ya, " + req.params(":name");
+        });
     }
 }
