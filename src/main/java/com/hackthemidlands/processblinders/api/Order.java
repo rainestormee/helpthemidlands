1e638af8ee483b5d196f0e846de8f273ff5f592f
Reuben Sinclair <reubenjsinclair@gmail.com>
2020-10-24 15:23:39 +0100

    add OrderUtil.java

--- src/main/java/com/hackthemidlands/processblinders/api/Order.java
+++ src/main/java/com/hackthemidlands/processblinders/api/Order.java
@@ -8,9 +8,15 @@ import java.util.List;
 @Data
 public class Order {
     private List<String> shopList = new ArrayList<>();
+    private int id;
     private double maxPrice;
     private String location;
     private OrderStatus status;
     private User user;
+
+    public int getID(){
+        return this.id;
+    }
     //time window?
+    // Time sent??
 }
