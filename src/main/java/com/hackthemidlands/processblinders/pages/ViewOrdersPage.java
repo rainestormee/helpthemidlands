2855f7db860bd05ca6d1787b04c31a80863a4c21
rainestormee <ryanarrowsmith01@gmail.com>
2020-10-24 23:06:20 +0100

    fixing login stuff

--- src/main/java/com/hackthemidlands/processblinders/pages/ViewOrdersPage.java
+++ src/main/java/com/hackthemidlands/processblinders/pages/ViewOrdersPage.java
@@ -19,22 +19,23 @@ public class ViewOrdersPage implements TemplateViewRoute {
     public ModelAndView handle(Request request, Response response) {
         User u = UserUtil.findUserFromDatabase(CookieUtil.getCookie(request));
         if (u == null) return new ErrorPage().handle(request, response); // should be redirected anyway before this point is hit
+        HashMap<String, Object> models = new HashMap<>();
+        models.put("user", u);
         if (u.isVolunteer()) {
-            return volunteerPage(request, response);
+            return volunteerPage(request, response, models);
         } else {
-            return userPage(request, response);
+            return userPage(request, response, models);
         }
     }
 
-    public ModelAndView volunteerPage(Request request, Response response) {
-        Map<String, Object> models = new HashMap<>();
+    public ModelAndView volunteerPage(Request request, Response response, Map<String, Object> models) {
         Order o = Order.builder().shopList(Arrays.asList("first item in first list", "second item in first list")).build();
         Order o2 = Order.builder().shopList(Arrays.asList("first item in second list", "second item in second list")).build();
         models.put("orders", Arrays.asList(o, o2));
-        return new ModelAndView(models, "orderVolunteerPage");
+        return new ModelAndView(models, "volunteerPage");
     }
 
-    public ModelAndView userPage(Request request, Response response) {
-        return new ErrorPage().handle(request, response);
+    public ModelAndView userPage(Request request, Response response, Map<String, Object> models) {
+        return new ModelAndView(models, "userPage");
     }
 }
