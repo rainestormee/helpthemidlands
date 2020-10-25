54bcc211fad8589b5a0497c4757bbe88342e4f70
rainestormee <ryanarrowsmith01@gmail.com>
2020-10-25 05:33:31 +0000

    Ryans Automagical Push Tool Because Hes Lazy

--- src/main/java/com/hackthemidlands/processblinders/pages/FrontPage.java
+++ src/main/java/com/hackthemidlands/processblinders/pages/FrontPage.java
@@ -1,16 +1,22 @@
 package com.hackthemidlands.processblinders.pages;
 
+import com.hackthemidlands.processblinders.util.CookieUtil;
+import com.hackthemidlands.processblinders.util.UserUtil;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 import spark.TemplateViewRoute;
 
+import javax.servlet.http.Cookie;
 import java.util.HashMap;
+import java.util.Map;
 
 public class FrontPage implements TemplateViewRoute {
 
     @Override
     public ModelAndView handle(Request request, Response response) {
-        return new ModelAndView(new HashMap<>(), "index");
+        Map<String, Object> models = new HashMap<>();
+        models.put("loggedIn", UserUtil.findUserFromDatabase(CookieUtil.getCookie(request)) != null);
+        return new ModelAndView(models, "index");
     }
 }
